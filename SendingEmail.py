97affff19490e2c26cab1f7ad97bbab1435a5588
HULLINGB <bradley.hullinger@ucdenver.edu>
2022-08-05 11:14:43 -0600

    Update to SendingEmail.py

--- SendingEmail.py
+++ SendingEmail.py
@@ -1,11 +1,13 @@
 import smtplib
 
-email = "example@gmail.com"
+email = "hullinger.brad@gmail.com"
 password = "PASSWORD"
+email2 = "hullinger.brad@gmail.com"
 message = "Choose message to send"
 
+#Host "gmail" rejects the correct password so smtplib package does connect to SMTP
 server = smtplib.SMTP(host="smtp.gmail.com", port=587)
 server.starttls()
 server.login(email, password)
-server.sendmail(email, email, message)
+server.sendmail(email, email2, message)
 server.quit()
